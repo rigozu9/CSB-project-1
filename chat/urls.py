e1d35278107c5855ecc383c96fc3a8823f74bb82
Riku <134082872+rigozu9@users.noreply.github.com>
2024-05-21 20:52:39 +0300

    introduced flaw 2 into the code SQL injection

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import chat_view, user_chat_view
+from .views import chat_view, user_chat_view, search_messages
 
 app_name = 'chat'  # Define the namespace
 
 urlpatterns = [
     path('', chat_view, name='chat'),
-    path('<str:username>/', user_chat_view, name='user_chat'),
+    path('search/', search_messages, name='search_messages'),
+    path('<str:username>/', user_chat_view, name='user_chat'), 
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.db import connection
 from .models import Message
 from .forms import MessageForm
 
@@ -31,12 +32,16 @@ def user_chat_view(request, username):
     # Fetch the user specified in the URL
     user = get_object_or_404(User, username=username)
 
-    # Check if the url link user is the same one as in the session. If not goes back to chat and pritns error
+    """
+        FLAW 1:  A01:2021-Broken Access Control 
+        Fix commented below this.
+        We have to check if the user from URL is the same as request.user.
+        Request user is the one whos logged in and he shouldnt be able to see other peoples messages.
+    """
     # if user != request.user:
     #     print("ERROR! STOP TRYING TO SEE OTHER PEOPELS MESSAGES")
     #     return redirect('chat:chat')  # Redirect to the chat view or show an error message
 
-    # Fetch messages where the user is the receiver or sender
     received_messages = Message.objects.filter(receiver=user).order_by('timestamp')
     sent_messages = Message.objects.filter(sender=user).order_by('timestamp')
 
@@ -46,3 +51,23 @@ def user_chat_view(request, username):
         'username': user.username,
         'current_user': request.user.username
     })
+
+@login_required
+def search_messages(request):
+    query = request.GET.get('query', '')
+    if query:
+        with connection.cursor() as cursor:
+            """
+                FLAW 2:  A03:2021-Injection 
+                Fix commented below the cursor.execute this.
+                When you search with input (' OR '1'='1) you get all the messages in the database.
+                You have to use parameterized SQL queries. 
+                %s is a placeholder in parameterized SQL queries
+            """
+            cursor.execute(f"SELECT * FROM chat_message WHERE content LIKE '%{query}%'")
+            #cursor.execute("SELECT * FROM chat_message WHERE content LIKE %s", [f'%{query}%'])
+            results = cursor.fetchall()
+    else:
+        results = []
+
+    return render(request, 'chat/search.html', {'results': results, 'query': query})
